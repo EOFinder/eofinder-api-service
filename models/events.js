b11ec09f93804cac8171d4e914f26a9cf320ef1d
Angga Prasetya W <agpw.id@gmail.com>
2020-09-08 13:39:02 +0800

    add events schema

---
+++ models/events.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const eventSchema = new Schema({
+    posted_by: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: String,
+        required: true
+    },
+    seats: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    isDone: Boolean,
+    required: false,
+    default: false
+}, { timestamps: true })
+
+const events = mongoose.model('events', movieSchema)
+
+module.exports = events;
